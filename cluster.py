db4a01ac2f2af9092d9c79699373fb69acf044b4
Sebastian Thomas <sebastianthomas1@live.com>
2015-10-18 00:42:32 +0530

    clusters

---
+++ cluster.py
@@ -0,0 +1,101 @@
+from random import randint
+import math
+
+def kmeans(mat, no_clusters):
+	temp_mat = mat
+	temp_clust = []
+	inital_centroids = []
+	i = no_clusters
+	while i > 0:
+		t = randint(1,len(temp_mat)) -1
+		if t not in inital_centroids:
+			inital_centroids.append(t)
+			temp_clust.append([])
+			temp_clust[no_clusters-i].append(t)
+			i = i - 1
+
+	for i in range(len(temp_mat)):
+		if i not in inital_centroids:
+			dist = find_dist(mat[inital_centroids[0]], mat[i] )
+			cent_clust = 0
+			j = -1
+			for centroid in inital_centroids:
+				j = j + 1
+				distew = find_dist(mat[centroid], mat[i])
+				print dist, distew
+				if distew < dist:
+					cent_clust = j
+			temp_clust[cent_clust].append(i)
+
+	#lets take cluster avg to make more accurate
+	clust_avg = find_clust_avg(mat,temp_clust)
+	temp_clust2 = [] 
+	for x in range(no_clusters):
+		temp_clust2.append([])
+
+	for i in range(len(temp_mat)):
+		dist = find_dist(clust_avg[0], mat[i] )
+		cent_clust = 0
+		for centroid in range(len(clust_avg)):
+			distew = find_dist(mat[i], clust_avg[centroid])
+			print dist, distew
+			if distew < dist:
+				cent_clust = centroid
+		temp_clust2[cent_clust].append(i)
+
+
+	print "One"
+	print temp_clust
+	print "Two"
+	print temp_clust2
+
+	return temp_clust2
+
+
+def find_clust_avg(mat,temp_clust):
+	new_clust_avg = []
+	for clust in temp_clust:
+		num_elems = len(clust)
+		tvect = [0.0 for x in range(len(mat[0]))]
+		if num_elems == 0 :
+			new_clust_avg.append(tvect)
+			continue
+		for elem in clust:
+			for i in range(len(mat[elem])):
+				tvect[i] = tvect[i] + mat[elem][i]
+
+		for i in range(len(tvect)):
+			tvect[i] = tvect[i] / num_elems
+		new_clust_avg.append(tvect)
+	return new_clust_avg
+
+
+
+def find_dist(p,q):
+	dist = 0
+	for i in range(len(p)):
+		dist = dist + (p[i] - q[i])* (p[i] - q[i])
+	return dist
+
+def find_similarity(p,q):
+	pq = 0.0
+	modp = 0.0
+	modq = 0.0
+	for col in p:
+		pq = pq + p[col]*q[col]
+
+	for col in p:
+		modp = modp + p[col] * p[col]
+	modp = math.sqrt(modp)
+	#print modp,p
+
+	for col in q:
+		modq = modq + q[col] * q[col]
+	modq = math.sqrt(modq)
+	#print modq,q
+	if modp == 0:
+		return 0
+	elif modq ==0:
+		return 0
+	else:
+		return pq /(modp * modq)
--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import facebook
 import requests
 import json
 import settings
+from random import randint
+import random
+from pprint import pprint
+import cluster
+
 
 users = {}
 graph = {}
 
 total_photos_fetched = 0
 max_photos = 200
-
+num_clust = 2
+access_token =''
 
 def add_photo_tags_to_graph(fb_photos):
 	global total_photos_fetched
@@ -40,25 +46,94 @@ def print_major_friends(user):
 		if val > 10 :
 			print key +" "+ str(val)
 
+def print_gangs(gangs):
+	for gang in gangs:
+		print gang
+
+def cluster_friends(major_friends_set):
+	mat = []
+	user_mat = []
+	dims = major_friends_set.keys()
+	for key,val in major_friends_set.items():
+		user_mat.append(key)
+		dim_vector = []
+		for d in dims:
+			if d in val:
+				dim_vector.append(1)
+			else:
+				dim_vector.append(0)
+		mat.append(dim_vector)
+
+	
+	temp_clust = cluster.kmeans(mat,num_clust)
+	for clust in temp_clust:
+		print "Cluster ----"
+		for p in clust:
+			print user_mat[p]
+
+	return temp_clust
+
+
+
 def get_major_friends(user):
 	major_friends = []
 	for key,val in graph[user].iteritems():
 		if val > 10 :
 			major_friends.append(key)
+	major_friends.sort()
 	return major_friends
 
 def get_major_friends_of_friends(user):
 	major_friends_set = {}
 	mlist = get_major_friends(user)
-	major_friends_set[user] = mlist
+	#major_friends_set[user] = mlist
 	for friend in mlist:
 		major_friends_set[friend] = get_major_friends(friend)
 	return major_friends_set
 
+def get_a_gang(num_of_players,major_friends_set):
+	num_itr = 0
+	while True:
+		num_itr = num_itr + 1
+		mjf_list = []
+		gang_list = []
+		mjf_list = major_friends_set.keys()
+		for x in range(0,num_of_players):
+			index = randint(1,len(mjf_list)) - 1
+			gang_list.append(mjf_list[index])
+			tmp_item = mjf_list[index]
+			#print str(num_of_players)+" : "+str(x)+" - "+str(tmp_item)
+			mjf_list.remove(tmp_item)
+		break_loop = True
+		for user1 in gang_list:
+			for user2 in gang_list:
+				if user1 != user2:
+					if user1 not in major_friends_set[user2]:
+						break_loop = False
+		if break_loop or num_itr > 10:
+			break
+	print num_itr
+	print gang_list
+
+def get_gangs_rand(major_friends_set):
+	num_itr = 0
+	all_close_friends = major_friends_set.keys()
+	gangs = []
+	done = False
+	while not done:
+		num_itr = num_itr + 1
+		rand_friend = random.choice(all_close_friends)
+		rand_friend_gang = list(set(all_close_friends).intersection(major_friends_set[rand_friend]))
+		if len(rand_friend_gang) > 0:
+			gangs.append(rand_friend_gang)
+			all_close_friends = [x for x in all_close_friends if x not in rand_friend_gang]
+		if len(all_close_friends) <= 0 or num_itr > 10 :
+			done = True
+	return gangs
 
 
 def main():
-	fb_graph = facebook.GraphAPI(settings.access_token)
+	fb_graph = facebook.GraphAPI(access_token)
 	fb_profile = fb_graph.get_object("me")
 	#friends = graph.get_connections("me", "friends")
 	args = {'fields' : 'id,tags', }
@@ -87,8 +162,27 @@ def main():
 	print len(graph.keys())
 	#print print_major_friends(fb_profile['name'])
 	print json.dumps(major_friends_set, indent=4, sort_keys=True)
-	
+	#get_a_gang(5,major_friends_set)
+	#get_a_gang(5,major_friends_set)
+	#get_a_gang(5,major_friends_set)
 
+	#print "Major Gangs"
+	#print "1"
+	#print_gangs(get_gangs_rand(major_friends_set))
+	#print "2"
+	#print_gangs(get_gangs_rand(major_friends_set))
+	#print "3"
+	#print_gangs(get_gangs_rand(major_friends_set))
+	tc = cluster_friends(major_friends_set)
+	return tc
+
+def run_pgm(access_token_t, num_clust_t):
+	global access_token
+	global num_clust
+	num_clust = num_clust_t
+	access_token = access_token_t
+	main()
 
 if __name__ == "__main__":
+	access_token = settings.access_token
 	main()
